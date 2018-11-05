2e775f072f401029648f8e41f2a260189c760ec3
daiki01240 <domo01240@gmail.com>
2018-11-05 23:17:28 +0900

    jsx Syntax

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-         <p>
-            Hello World.
-          </p>
-    </div>
-    );
-  }
+
+   return (
+   <React.Fragment>
+   <label htmlFor="bar">bar</label>
+   <input type="text" onChange={() => {console.log("I am clicked")}} />
+       </React.Fragment>
+   );
+    }
 }
 
 export default App;
